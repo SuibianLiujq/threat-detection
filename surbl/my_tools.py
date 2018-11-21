766c26afd398d3a34869bc055c1a860bbc3b60ff
songh <sh9369@users.noreply.github.com>
2018-11-21 19:41:01 +0800

    fix import

--- surbl/my_tools.py
+++ surbl/my_tools.py
@@ -7,7 +7,7 @@
 # all functions are implemented in this file.
 
 import sys,os
-sys.path.append(os.path.abspath(os.path.pardir))
+sys.path.append("../global_tools")
 import json,os,time,datetime
 from global_tools import set_logger
 from ip import check_XForce as xf
